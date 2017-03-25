6eb8f49b3946982018284af3dd003b3f8fffb54b
Toma Morisawa <morisawa@mizuuchi.lab.tuat.ac.jp>
2017-03-25 13:46:29 +0900

    画像のプリロード

--- hiroshijs/hiroshi.js
+++ hiroshijs/hiroshi.js
@@ -1,26 +1,16 @@
-// スクロール量の割合を取得
-function getScrollRatio() {
-    var body = window.document.body;
-    var html = window.document.documentElement;
-    var scrollTop = body.scrollTop || html.scrollTop;
-    return scrollTop / (html.scrollHeight - html.clientHeight);
-}
-
-// 発火
-function forcedScroll(){
-    // 強制スクロール
-    $('html, body').animate({scrollTop:0}, 400, 'swing'); //slow, fast, or NNN
-
-    // ヒロシの画像を変更
-    $('.hiroshi').html('<img src="./hiroshijs/hiroshi2.png" width="100">');
-}
 
 // main
 $(function(){
+    // hiroshiの画像をプリロード（キャッシュに保存）する
+    $("<img>").attr("src", "./hiroshijs/hiroshi2.png" );
+    
     // hiroshi挿入
     $('body').append('<div class="hiroshi"></div>')
-    // hiroshiの位置を固定
-    // bottom: ウィンドウ下からの位置, right: 右からの位置
+
+    /*
+      hiroshiの位置を固定
+      bottom: ウィンドウ下からの位置, right: 右からの位置
+    */
     $('.hiroshi').css({"position":"fixed","bottom":'100px',"right":"100px"});
     $('.hiroshi').html('<img src="./hiroshijs/hiroshi1.png" width="100">');
 
@@ -36,3 +26,20 @@ $(function(){
         }
     });
 });
+
+// スクロール量の割合を取得
+function getScrollRatio() {
+    var body = window.document.body;
+    var html = window.document.documentElement;
+    var scrollTop = body.scrollTop || html.scrollTop;
+    return scrollTop / (html.scrollHeight - html.clientHeight);
+}
+
+// 発火
+function forcedScroll(){
+    // ヒロシの画像を変更
+    $('.hiroshi img').attr('src', './hiroshijs/hiroshi2.png');
+
+    // 強制スクロール
+    $('html, body').animate({scrollTop:0}, 400, 'swing'); //slow, fast, or NNN
+}
